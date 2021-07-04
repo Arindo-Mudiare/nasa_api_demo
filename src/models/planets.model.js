d59ced3529f8d3ab64a18759838894bc304ce473
Irapfaya <arindomudiare@gmail.com>
2021-07-04 14:45:34 +0100

    abstracted model functionality from controller functionality

--- src/models/planets.model.js
+++ src/models/planets.model.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const path = require("path");
 const parse = require("../../node_modules/csv-parse/lib");
 
-
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
@@ -15,36 +14,38 @@ function isHabitablePlanet(planet) {
 }
 
 function loadPlanetsData() {
-
   return new Promise((resolve, reject) => {
-    fs.createReadStream(path.join(__dirname,'..','..','data','kepler_data.csv'))
-    .pipe(
-      parse({
-        comment: "#", 
-        columns: true,
-      })
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
     )
-    .on("data", (data) => {
-      if (isHabitablePlanet(data)) {
-        habitablePlanets.push(data);
-      }
-    })
-    .on("error", (err) => {
-      console.log(err);
-      reject(err);
-    })
-    .on("end", () => {
-      console.log(
-        `done parsing csv! we found ${habitablePlanets.length} habitable planets!!`
-      );
-      resolve();
-    });
-  })
-  
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(
+          `done parsing csv! we found ${habitablePlanets.length} habitable planets!!`
+        );
+        resolve();
+      });
+  });
 }
-
+function getAllPlanets() {
+  return habitablePlanets;
+}
 
 module.exports = {
   loadPlanetsData,
-  planets: habitablePlanets,
+  getAllPlanets,
 };
